e333b8cb9fd4968300fb08c47700c5b952ce3e4f
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-01-27 18:29:21 +0200

    Added Materialize CSS, JS, and Material Icons.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+// Importing Materialize CSS and JS
+
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min';
+
+// Importing main partial of custom SASS
 import './scss/main.scss';
