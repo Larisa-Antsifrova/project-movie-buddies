215d3d2f3b246d386ecbc78630916e0e8187f3b5
Irina-Blazhevich <blazhevych.iryna@gmail.com>
2021-01-28 21:14:50 +0200

    added footer html markup

--- src/js/templates.js
+++ src/js/templates.js
@@ -3,3 +3,9 @@ import headerTemplate from '../templates/1header.hbs';
 const headerMarkup = headerTemplate();
 const headerRef = document.querySelector('.header__js');
 headerRef.insertAdjacentHTML('beforeend', headerMarkup);
+
+//Подключение Footer
+import footerTemplate from '../templates/7footer.hbs';
+const footerMarkup = footerTemplate();
+const footerRef = document.querySelector('.footer__js');
+footerRef.insertAdjacentHTML('beforeend', footerMarkup);
