3764498931ff391eaa07a36a01e64aa33e1618d1
arestus <arestus@gmail.com>
2021-01-28 17:31:25 +0200

    Refactored code for templates.js

--- src/js/templates.js
+++ src/js/templates.js
@@ -1,11 +1,5 @@
+//Подключение header'а
 import headerTemplate from '../templates/1header.hbs';
-import homeTemplate from '../templates/2home.hbs';
-
-const inputMarkup = homeTemplate();
 const headerMarkup = headerTemplate();
-
-const homeRef = document.querySelector('.home__js');
 const headerRef = document.querySelector('.header__js');
-
-homeRef.insertAdjacentHTML('beforeend', inputMarkup);
 headerRef.insertAdjacentHTML('beforeend', headerMarkup);
