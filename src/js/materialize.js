8be8598e8ac676207de41b67e34e0af50e3ce8bc
Julia Lavrenko <lavrenkojulia@gmail.com>
2021-01-28 20:16:09 +0100

    Initialized tabs

--- src/js/materialize.js
+++ src/js/materialize.js
@@ -3,6 +3,8 @@ import 'materialize-css/dist/js/materialize.min';
 // Здесь происходит инициализация компонентов materialize'а
 // Setting up materialize components
 document.addEventListener('DOMContentLoaded', function () {
+  const tabs = document.querySelectorAll(".tabs");
+  M.Tabs.init(tabs);
   var modals = document.querySelectorAll('.modal');
   M.Modal.init(modals);
 });
