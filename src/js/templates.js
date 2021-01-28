4840d533cb80e3a244eb06502adda53be47345b5
Julia Lavrenko <lavrenkojulia@gmail.com>
2021-01-28 18:10:48 +0100

    Library page and GalleryElement are raedy

--- src/js/templates.js
+++ src/js/templates.js
@@ -1,18 +1,17 @@
-// import headerTemplate from '../templates/1header.hbs';
-
-// Initialize Home Page 
+// Initialize Home Page
 import homeTemplate from '../templates/2home.hbs';
 const inputMarkup = homeTemplate();
 const homeRef = document.querySelector('.home__js');
 homeRef.insertAdjacentHTML('beforeend', inputMarkup);
 
-// Initialize Library Page 
+// Initialize Library Page
 import libraryTemplate from '../templates/3library.hbs';
-const libraryRef = document.querySelector('.library__js');
 const libraryMarkup = libraryTemplate();
+const libraryRef = document.querySelector('.library__js');
 libraryRef.insertAdjacentHTML('beforeend', libraryMarkup);
-// const galleryElementMarkup = galleryElementTemplate();
 
-import galleryElement from '../templates/8galleryElement.hbs';
-// const galleryElementRef = document.c
-
+// Initialize GalleryElement
+import galleryElementTemplate from '../templates/8galleryElement.hbs';
+const galleryElementMarkup = galleryElementTemplate();
+const galleryElementRef = document.getElementById('library-gallery');
+galleryElementRef.insertAdjacentHTML('beforeend', galleryElementMarkup);
