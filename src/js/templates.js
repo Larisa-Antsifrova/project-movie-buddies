f8f14bac22e69cdd6fb3f4fbcac1114d037bdad8
Anna Sokolova <sokolovaskorik@gmail.com>
2021-01-28 20:59:10 +0200

    Added the base style for container and section

--- src/js/templates.js
+++ src/js/templates.js
@@ -3,3 +3,9 @@ import headerTemplate from '../templates/1header.hbs';
 const headerMarkup = headerTemplate();
 const headerRef = document.querySelector('.header__js');
 headerRef.insertAdjacentHTML('beforeend', headerMarkup);
+
+//Подключение разметки модального окна с деталями фильма
+import detailTemplate from '../templates/4details.hbs';
+const detailMarkup = detailTemplate();
+const detailRef = document.querySelector('.details__js');
+detailRef.insertAdjacentHTML('beforeend', detailMarkup);
