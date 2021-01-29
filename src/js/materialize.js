f0edbd99102903d63ee90ed54b4d0ed2de5ccc23
Anna Sokolova <sokolovaskorik@gmail.com>
2021-01-29 15:58:15 +0200

    Added a function for drawing details

--- src/js/materialize.js
+++ src/js/materialize.js
@@ -1,10 +1,18 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min';
+import { showDetails } from './show-details.js';
 // Здесь происходит инициализация компонентов materialize'а
 // Setting up materialize components
 document.addEventListener('DOMContentLoaded', function () {
-  const tabs = document.querySelectorAll(".tabs");
+  const tabs = document.querySelectorAll('.tabs');
   M.Tabs.init(tabs);
   var modals = document.querySelectorAll('.modal');
   M.Modal.init(modals);
+  // const detailsModal = document.getElementById('details-modal');
+  // M.Modal.init(detailsModal, {
+  //   onOpenEnd: e => {
+  //     console.log('test event', e);
+  //     // showDetails(e);
+  //   },
+  // });
 });
---
+++ src/js/show-details.js
@@ -0,0 +1,22 @@
+import { currentMovieItem, currentMoviesList, genres } from './movieApi.js';
+// console.log(currentMovieItem, currentMoviesList, genres);
+const homeGalleryRef = document.querySelector('.home__js');
+const titleFilmRef = document.querySelector('.title-film__js');
+console.log(titleFilmRef);
+
+homeGalleryRef.addEventListener('click', e => {
+  showDetails(e);
+});
+
+async function showDetails(e) {
+  const id = +e.target.dataset.id;
+
+  let movieList = await currentMoviesList;
+  let currentMovieItem = movieList.find(el => el.id === id);
+  let movieTitle = await currentMovieItem.title || currentMovieItem.name;
+  console.log(currentMovieItem);
+
+  titleFilmRef.textContent = movieTitle;
+}
+
+export { showDetails };
