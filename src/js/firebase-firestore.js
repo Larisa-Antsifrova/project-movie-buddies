1b5db08fabba5f12e15415515d23b9cc20febd4c
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-01-29 13:00:18 +0200

    Added function to add a movie to watched.

--- src/js/firebase-firestore.js
+++ src/js/firebase-firestore.js
@@ -1,3 +1,55 @@
+// Imports of firestore services and variables
+import { db, auth } from './firebase-init';
 import { currentMoviesList, currentMovieItem, genres } from './movieApi';
 
-console.log(currentMoviesList, currentMovieItem, genres);
+// Getting references to DOM elements
+const watchedBtnRef = document.querySelector('.watched-btn__js');
+const queueBtnRef = document.querySelector('.queue-btn__js');
+const favoriteBtnRef = document.querySelector('.favorite-btn__js');
+
+console.log(watchedBtnRef, queueBtnRef, favoriteBtnRef);
+
+// Function to set buttons UI
+function updateBtn() {
+  const user = auth.currentUser;
+
+  db.collection('users')
+    .doc(user.uid)
+    .collection('watched')
+    .doc('85271')
+    .get()
+    .then(docSnapshot => {
+      if (docSnapshot.exists) {
+        watchedBtnRef.textContent = 'Remove';
+      }
+    });
+}
+
+watchedBtnRef.addEventListener('click', addToWatched);
+
+// Function to add to Watched
+async function addToWatched() {
+  updateBtn();
+  const user = auth.currentUser;
+  let currentMovieItem = await currentMoviesList.then(array =>
+    array.find(item => item.id === 85271),
+  );
+
+  console.log('currentMovieItem in addWatched', currentMovieItem);
+  console.log('currentMoviesList in Watched', currentMoviesList[0]);
+
+  if (watchedBtnRef.dataset.status === 'add') {
+    db.doc(`users/${user.uid}/watched/${currentMovieItem.id}`)
+      .set(currentMovieItem)
+      .then(() => {
+        console.log('Document successfully written!');
+      })
+      .catch(error => console.log(error.message));
+  } else {
+    db.doc(`users/${user.uid}/watched/${currentMovieItem.id}`)
+      .delete()
+      .then(() => {
+        console.log('movie deleted');
+      });
+  }
+}
