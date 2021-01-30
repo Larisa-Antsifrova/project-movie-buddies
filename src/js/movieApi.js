135947c01b3bb983926cce09977210caffddc778
AlexBelozertsev <kashim983@gmail.com>
2021-01-30 21:46:23 +0200

    input & paginator

--- src/js/movieApi.js
+++ src/js/movieApi.js
@@ -119,7 +119,7 @@ async function createMovieList(moviesList) {
   const galleryListMarkup = galleryElementTemplate(fullInfo);
   homeGalleryRef.insertAdjacentHTML('beforeend', galleryListMarkup);
 }
-createMovieList(currentMoviesList);
+// createMovieList(currentMoviesList);
 
 async function getGenresInfo(moviesList) {
   const genresInfo = await Promise.all([moviesList, genres]);
@@ -149,8 +149,8 @@ Handlebars.registerHelper('getMovieYear', function (release_date) {
 
 Handlebars.registerHelper('getPoster', function (poster_path) {
   if (!poster_path) {
-    const defaultImgUrl = 'https://drive.google.com/file/d/1pdAzALp81F5lBAVna3VrQKfcVaMirqrQ/view?usp=sharing'
-    return Handlebars.SafeString(defaultImgUrl);
+    const defaultImgUrl = `${Api.images.baseImageUrl}${Api.images.currentSizes.posterSize}/wwemzKWzjKYJFfCeiB57q3r4Bcm.png`;
+    return defaultImgUrl;
   } else {
     const imgUrl = `${Api.images.baseImageUrl}${Api.images.currentSizes.posterSize}/${poster_path}`
     return imgUrl;
@@ -158,12 +158,7 @@ Handlebars.registerHelper('getPoster', function (poster_path) {
   }
 });
 
-
-// ==================================================
-export { Api, createMovieList, currentMoviesList, currentMovieItem, genres };
-  
 // ==================================== input ===============================================================
-
 searchForm.addEventListener('click', onInputFocus);
 searchForm.addEventListener('submit', searchFilms);
 
@@ -187,13 +182,13 @@ function toggleRenderPage() {
 // функция рендера страницы запроса
 function renderSearchedFilms(inputValue) {
   currentMoviesList = Api.fetchSearchMovieList(inputValue);
-  createMovieList(currentMoviesList);
+  return createMovieList(currentMoviesList);
 };
 
 // функция рендера страницы трендов
 function renderPopularFilms() {
   currentMoviesList = Api.fetchTrendingMoviesList();
-  createMovieList(currentMoviesList);
+  return createMovieList(currentMoviesList);
 };
 
 function clearGallery(filmsList) {
@@ -225,3 +220,351 @@ function clearError() {
   errorArea.style.visibility = 'hidden';
 }
 
+// ======================== paginator ========================
+class PaginationApi {
+  constructor(totalPages = 1, displayNumber = 5) {
+    this.paginationContainerRef = document.querySelector('.pagination-container');
+    this.paginationPageItemsContainerRef = document.querySelector('.pagination-page-items-container');
+
+    this.paginationToBeginningBtnRef = document.querySelector('.pagination-beginning');
+    this.paginationToBeginningIconRef = this.paginationToBeginningBtnRef.querySelector('i');
+    this.paginationPreviousPageRef = document.querySelector('.pagination-previous-page');
+    this.paginationPreviousPageIconRef = this.paginationPreviousPageRef.querySelector('i');
+
+    this.paginationToEndBtnRef = document.querySelector('.pagination-end');
+    this.paginationToEndIconRef = this.paginationToEndBtnRef.querySelector('i');
+    this.paginationNextPageRef = document.querySelector('.pagination-next-page');
+    this.paginationNextPageIconRef = this.paginationNextPageRef.querySelector('i');
+
+    this.totalPages = totalPages;
+    this.displayNumber = displayNumber;
+    this.lowRange = 1;
+    this.upRange = this.totalPages > this.displayNumber ? this.lowRange + this.displayNumber - 1 : this.totalPages;
+    this.totalPaginationBatches = Math.ceil(this.totalPages / this.displayNumber);
+    this.currentPaginationBatch = 1;
+    this.isLastPaginationBatch = false;
+    this.isFirstPaginationBatch = true;
+    this.currentActivePage = null;
+
+    this.paginationContainerRef.addEventListener('click', this.onPaginationClick.bind(this));
+  }
+
+  clearPaginationPageItems() {
+    this.paginationPageItemsContainerRef.innerHTML = '';
+  }
+
+  createPaginationPageItemsMarkup() {
+    let paginationPageItemsMarkup = '';
+    for (let i = this.lowRange; i <= this.upRange; i++) {
+      paginationPageItemsMarkup += `<li class="waves-effect" data-page ='${i}'>
+              <a href="#!" class="paginator-page-item" data-page ='${i}'>${i}</a>
+            </li>`;
+    }
+    return paginationPageItemsMarkup;
+  }
+
+  renderPaginationPageItems() {
+    const paginationPageItemsMarkup = this.createPaginationPageItemsMarkup();
+    this.paginationPageItemsContainerRef.insertAdjacentHTML('afterbegin', paginationPageItemsMarkup);
+
+    const totalPageItems = this.paginationPageItemsContainerRef.querySelectorAll('li');
+
+    if (totalPageItems.length === 1) {
+      this.disableToBeginningBtn();
+      this.disablePreviousPageBtn();
+      this.disableToEndBtn();
+      this.disableNextPageBtn();
+      this.assignCurrentActivePage();
+      return;
+    }
+
+    if (Api.pageNumber === 1) {
+      this.disableToBeginningBtn();
+      this.disablePreviousPageBtn();
+      // this.assignCurrentActivePage();
+      this.enableToEndBtn();
+      this.enableNextPageBtn();
+    }
+
+    if (Api.pageNumber === this.totalPages) {
+      this.disableToEndBtn();
+      this.disableNextPageBtn();
+
+      this.enableToBeginningBtn();
+      this.enablePreviousPageBtn();
+    }
+
+    this.assignCurrentActivePage();
+  }
+
+  disableToEndBtn() {
+    this.paginationToEndBtnRef.classList.add('disabled');
+    this.paginationToEndIconRef.setAttribute('disabled', true);
+  }
+  enableToEndBtn() {
+    this.paginationToEndBtnRef.classList.remove('disabled');
+    this.paginationToEndIconRef.removeAttribute('disabled');
+  }
+  disableNextPageBtn() {
+    this.paginationNextPageRef.classList.add('disabled');
+    this.paginationNextPageIconRef.setAttribute('disabled', true);
+  }
+  enableNextPageBtn() {
+    this.paginationNextPageRef.classList.remove('disabled');
+    this.paginationNextPageIconRef.removeAttribute('disabled');
+  }
+  disableToBeginningBtn() {
+    this.paginationToBeginningBtnRef.classList.add('disabled');
+    this.paginationToBeginningIconRef.setAttribute('disabled', true);
+  }
+  enableToBeginningBtn() {
+    this.paginationToBeginningBtnRef.classList.remove('disabled');
+    this.paginationToBeginningIconRef.removeAttribute('disabled');
+  }
+  disablePreviousPageBtn() {
+    this.paginationPreviousPageRef.classList.add('disabled');
+    this.paginationPreviousPageIconRef.setAttribute('disabled', true);
+  }
+  enablePreviousPageBtn() {
+    this.paginationPreviousPageRef.classList.remove('disabled');
+    this.paginationPreviousPageIconRef.removeAttribute('disabled');
+  }
+
+  assignCurrentActivePage() {
+      const paginationPageItems = this.paginationPageItemsContainerRef.querySelectorAll('li');
+      const liArr = Array.from(paginationPageItems);
+      const activeTarget = liArr.find(node => +node.dataset.page === Api.pageNumber);
+    activeTarget.classList.add('active');
+    this.currentActivePage = activeTarget;
+  }
+
+  switchCurrentActivePage() {
+    const paginationPageItems = this.paginationPageItemsContainerRef.querySelectorAll('li');
+    const liArr = Array.from(paginationPageItems);
+    const activeTarget = liArr.find(node => +node.dataset.page === Api.pageNumber);
+    this.currentActivePage.classList.remove('active');
+    activeTarget.classList.add('active');
+    this.currentActivePage = activeTarget;
+  }
+
+  goToBeginning() {
+    this.currentPaginationBatch = 1;
+    this.isFirstPaginationBatch = true;
+
+    Api.resetPage();
+
+    this.lowRange = 1;
+    this.upRange = this.totalPages > this.displayNumber ? this.lowRange + this.displayNumber - 1 : this.totalPages;
+
+    this.clearPaginationPageItems();
+    this.renderPaginationPageItems();
+
+    this.disableToBeginningBtn();
+    this.disablePreviousPageBtn();
+
+    this.enableToEndBtn();
+    this.enableNextPageBtn();
+
+    this.assignCurrentActivePage();
+  }
+
+  goToEnd() {
+    this.currentPaginationBatch = this.totalPaginationBatches;
+    this.isLastPaginationBatch = true;
+
+    Api.pageNumber = this.totalPages;
+    this.upRange = this.totalPages;
+    this.lowRange = this.totalPages > this.displayNumber ? this.upRange - this.displayNumber + 1 : 1;
+
+    this.clearPaginationPageItems();
+    this.renderPaginationPageItems();
+
+    this.disableToEndBtn();
+    this.disableNextPageBtn();
+
+    this.enableToBeginningBtn();
+    this.enablePreviousPageBtn();
+
+    this.assignCurrentActivePage();
+  }
+
+  goToNextPage() {
+    this.enableToBeginningBtn();
+    this.enablePreviousPageBtn();
+
+    Api.incrementPage();
+
+    if (Api.pageNumber > this.totalPages) {
+      Api.pageNumber = this.totalPages;
+      return;
+    }
+
+    if (Api.pageNumber === this.totalPages) {
+      this.disableToEndBtn();
+      this.disableNextPageBtn();
+    }
+
+    if (Api.pageNumber > this.upRange) {
+      this.currentPaginationBatch += 1;
+      this.isLastPaginationBatch = this.currentPaginationBatch === this.totalPaginationBatches;
+
+      if (this.isLastPaginationBatch) {
+        this.upRange = this.totalPages;
+        this.lowRange = this.totalPages > this.displayNumber ? this.upRange - this.displayNumber + 1 : 1;
+
+        this.clearPaginationPageItems();
+        this.renderPaginationPageItems();
+
+        this.switchCurrentActivePage();
+
+        return;
+      }
+
+      this.lowRange = this.upRange + 1;
+      this.upRange =
+        this.totalPages > this.displayNumber * this.currentPaginationBatch
+          ? this.lowRange + this.displayNumber - 1
+          : this.totalPages;
+
+      this.clearPaginationPageItems();
+      this.renderPaginationPageItems();
+    }
+
+    this.switchCurrentActivePage();
+  }
+
+  goToPreviousPage() {
+    this.enableToEndBtn();
+    this.enableNextPageBtn();
+
+    Api.decrementPage();
+
+    if (Api.pageNumber < 1) {
+      Api.resetPage();
+      return;
+    }
+
+    if (Api.pageNumber === 1) {
+      this.disableToBeginningBtn();
+      this.disablePreviousPageBtn();
+    }
+
+    if (Api.pageNumber < this.lowRange) {
+      this.currentPaginationBatch -= 1;
+      this.isFirstPaginationBatch = this.currentPaginationBatch === 1;
+
+      if (this.isFirstPaginationBatch) {
+        this.lowRange = 1;
+        this.upRange = this.totalPages > this.displayNumber ? this.lowRange + this.displayNumber - 1 : this.totalPages;
+
+        this.clearPaginationPageItems();
+        this.renderPaginationPageItems();
+
+        this.assignCurrentActivePage();
+
+        return;
+      }
+
+      this.upRange = this.lowRange - 1;
+      this.lowRange = this.upRange - this.displayNumber + 1;
+
+      this.clearPaginationPageItems();
+      this.renderPaginationPageItems();
+    }
+
+    this.switchCurrentActivePage();
+  }
+
+  goToSelectedPage(e) {
+    Api.pageNumber = +e.target.dataset.page;
+
+    if (Api.pageNumber === 1) {
+      this.disableToBeginningBtn();
+      this.disablePreviousPageBtn();
+      this.enableToEndBtn();
+      this.enableNextPageBtn();
+    }
+
+    if (Api.pageNumber === this.totalPages) {
+      this.disableToEndBtn();
+      this.disableNextPageBtn();
+      this.enableToBeginningBtn();
+      this.enablePreviousPageBtn();
+    }
+
+    if (Api.pageNumber !== 1 && Api.pageNumber !== this.totalPages) {
+      this.enableToBeginningBtn();
+      this.enablePreviousPageBtn();
+      this.enableToEndBtn();
+      this.enableNextPageBtn();
+    }
+
+    this.switchCurrentActivePage();
+  }
+
+  onPaginationClick(e) {
+    if (e.target.nodeName !== 'A' && e.target.nodeName !== 'I') {
+      return;
+    }
+
+    if (e.target.hasAttribute('disabled')) {
+      return;
+    }
+
+    if (e.target.nodeName === 'A' && e.target.classList.contains('paginator-page-item')) {
+      this.goToSelectedPage(e);
+      toggleRenderPage();
+      return;
+    }
+
+    if (e.target.textContent === 'first_page') {
+      this.goToBeginning();
+      toggleRenderPage();
+      return;
+    }
+
+    if (e.target.textContent === 'chevron_left') {
+      this.goToPreviousPage();
+      toggleRenderPage();
+      return;
+    }
+
+    if (e.target.textContent === 'last_page') {
+      this.goToEnd();
+      toggleRenderPage();
+      return;
+    }
+
+    if (e.target.textContent === 'chevron_right') {
+      this.goToNextPage();
+      toggleRenderPage();
+      return;
+    }
+  }
+
+  recalculate(newTotalPages) {
+    this.totalPages = newTotalPages;
+
+    this.lowRange = 1;
+    this.upRange = this.totalPages > this.displayNumber ? this.lowRange + this.displayNumber - 1 : this.totalPages;
+
+    this.totalPaginationBatches = Math.ceil(this.totalPages / this.displayNumber);
+    this.isLastPaginationBatch = false;
+    this.isFirstPaginationBatch = true;
+
+    this.clearPaginationPageItems();
+    this.renderPaginationPageItems();
+  }
+}
+
+// Инициализация пагинатора
+const paginator = new PaginationApi(Api.totalPages);
+
+// Вызов самого первого fetch за популярными фильмами и его рендер
+renderPopularFilms().then(() => {
+  paginator.recalculate(Api.totalPages);
+});
+
+
+// ==================================================
+export { Api, currentMoviesList, currentMovieItem, genres };
