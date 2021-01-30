46e9b797978dd5709a188443b80a14f56dfb0319
arestus <arestus@gmail.com>
2021-01-30 21:35:00 +0200

    Add logic for sidenav

--- src/js/materialize.js
+++ src/js/materialize.js
@@ -19,3 +19,8 @@ document.addEventListener('DOMContentLoaded', function () {
   //   },
   // });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, { edge: 'left', draggable: true });
+});
