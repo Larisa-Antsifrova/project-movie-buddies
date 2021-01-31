dcc9ead4c7df8accdee0de13dfc52cd8f843458c
Larisa <67143768+Larisa-Antsifrova@users.noreply.github.com>
2021-02-01 01:36:20 +0200

    Revert "Revert "Mobile menu sidenav""

--- src/js/materialize.js
+++ src/js/materialize.js
@@ -9,6 +9,8 @@ document.addEventListener('DOMContentLoaded', function () {
   M.Tabs.init(tabs);
   var modals = document.querySelectorAll('.modal');
   M.Modal.init(modals);
+  var elems = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(elems, { edge: 'right', draggable: true });
   // activeModal = M.Modal.getInstance('#details-modal');
   // activeModal.open();
   // const detailsModal = document.getElementById('details-modal');
