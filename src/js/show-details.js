2b36c5f66e9999acd2716d61b6fedcd96a5dd35a
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-02-02 02:23:13 +0200

    Reorganized variables and functions in details.

--- src/js/show-details.js
+++ src/js/show-details.js
@@ -1,6 +1,6 @@
+// Imports of firestore services, template, and variables
 import { auth, db } from './firebase-init';
 import { currentMoviesList } from './fetch-functions.js';
-import * as Handlebars from 'handlebars/runtime';
 import detailTemplate from '../templates/4details.hbs';
 import {
   watchedBtnRef,
@@ -13,21 +13,12 @@ import {
   updateFavoriteCollectionBtn,
 } from './firebase-firestore.js';
 
-let currentMovieItem = {};
-
-const homeGalleryRef = document.querySelector('.home-gallery-list__js'); //доступ к ul галлереи для слушателя модалки
-const sectionDetails = document.querySelector('.details__js'); // доступ к секции с деталями в html
-const titleFilmRef = document.querySelector('.title-film__js');
-const overviewRef = document.querySelector('.overview__js');
-const popularityRef = document.querySelector('.popularity__js');
-const releaseDateRef = document.querySelector('.release-date__js');
-const voteRef = document.querySelector('.vote__js');
-const votesRef = document.querySelector('.votes__js');
-const originalTitleRef = document.querySelector('.original-title__js');
-const modalContent = document.querySelector('.modal-content__js');
-
+//Getting access to DOM elements
+const homeGalleryRef = document.querySelector('.home-gallery-list__js');
 const innerModalRef = document.querySelector('.test-drive_js');
-const detailsModalRef = document.querySelector('#details-modal'); //доступ к модалке
+
+//Global lonely variable, but super important one :)
+let currentMovieItem = {};
 
 // Adding event listeners
 homeGalleryRef.addEventListener('click', onDetailsModalOpen);
@@ -35,6 +26,7 @@ watchedGalleryRef.addEventListener('click', onDetailsModalOpen);
 queueGalleryRef.addEventListener('click', onDetailsModalOpen);
 favoriteGalleryRef.addEventListener('click', onDetailsModalOpen);
 
+// Function to carry out open modal scenario
 async function onDetailsModalOpen(e) {
   if (e.target.nodeName === 'UL') {
     return;
@@ -56,16 +48,20 @@ async function onDetailsModalOpen(e) {
   showDetails(e, currentMovieItem);
 }
 
+// Function to render movie details in details modal
 async function showDetails(e, currentMovieItem) {
   e.preventDefault();
 
-  if (e.target.parentElement.nodeName !== 'A') {
+  if (e.target.parentElement.nodeName !== 'A' && e.target.parentElement.nodeName !== 'LI') {
     return;
   }
 
-  renderMovieDetails(currentMovieItem);
+  innerModalRef.innerHTML = '';
+  const modalMarkup = detailTemplate(currentMovieItem);
+  innerModalRef.insertAdjacentHTML('afterbegin', modalMarkup);
 }
 
+// Funtion to get the current movie selected for review
 async function getCurrentMovieItem(e, user, id) {
   if (e.currentTarget.classList.contains('home-gallery-list__js')) {
     let movieList = await currentMoviesList;
@@ -89,15 +85,21 @@ async function getCurrentMovieItem(e, user, id) {
   }
 }
 
-function renderMovieDetails(currentMovieItem) {
-  innerModalRef.innerHTML = '';
-  const modalMarkup = detailTemplate(currentMovieItem);
-  innerModalRef.insertAdjacentHTML('afterbegin', modalMarkup);
-}
-
 export { currentMovieItem, innerModalRef };
 
 // Закомментированные ранее отрывки кода
+// import * as Handlebars from 'handlebars/runtime';
+// const sectionDetails = document.querySelector('.details__js'); // доступ к секции с деталями в html
+// const titleFilmRef = document.querySelector('.title-film__js');
+// const overviewRef = document.querySelector('.overview__js');
+// const popularityRef = document.querySelector('.popularity__js');
+// const releaseDateRef = document.querySelector('.release-date__js');
+// const voteRef = document.querySelector('.vote__js');
+// const votesRef = document.querySelector('.votes__js');
+// const originalTitleRef = document.querySelector('.original-title__js');
+// const modalContent = document.querySelector('.modal-content__js');
+// const detailsModalRef = document.querySelector('#details-modal'); //доступ к модалке
+
 // Handlebars.registerHelper('getMovieYear', function (release_date) {
 //   if (!release_date) {
 //     return;
