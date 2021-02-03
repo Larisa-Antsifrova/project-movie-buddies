64313210925058a71e9ff8824e75655a5f0662cc
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-02-03 17:34:17 +0200

    Added conditions for mobile and desktop telegram links.

--- src/js/firebase-buddy.js
+++ src/js/firebase-buddy.js
@@ -38,88 +38,95 @@ function findBuddy(e) {
 }
 
 function renderBuddy(doc, fragment, userId, movieId) {
+  // Getting info to fill in Buddy search result
   const name = doc.data().name;
   const email = doc.data().email;
   const telegram = doc.data().telegram;
 
+  // Creating container to contain Buddy's info
   const li = document.createElement('li');
   li.classList.add('collection-item', 'row', 'valign-wrapper');
 
+  // Creating element to hold Buddy's name
   const nameSpan = document.createElement('span');
   nameSpan.textContent = name;
   nameSpan.classList.add('col', 's4');
 
+  // Creating element to contain collection chips
   const collectionDiv = document.createElement('div');
   collectionDiv.classList.add('col', 's4');
-  collectionDiv.classList.add('center');
 
-  const watchedSpan = document.createElement('span');
-  watchedSpan.classList.add('chip', 'orange', 'darken-2', 'white-text');
-  watchedSpan.textContent = 'watched';
-  const queueSpan = document.createElement('span');
-  queueSpan.textContent = 'queue';
-  queueSpan.classList.add('chip', 'orange', 'darken-2', 'white-text');
-
-  const favoriteSpan = document.createElement('span');
-  favoriteSpan.textContent = 'favorite';
-  favoriteSpan.classList.add('chip', 'orange', 'darken-2', 'white-text');
-
+  // Checking conditions for each collection to render corresponding chips
   isInCollection(userId, 'watched', movieId).then(reply => {
     if (reply) {
+      const watchedSpan = document.createElement('span');
+      watchedSpan.classList.add('chip', 'orange', 'darken-2', 'white-text');
+      watchedSpan.textContent = 'watched';
       collectionDiv.appendChild(watchedSpan);
     }
   });
 
   isInCollection(userId, 'queue', movieId).then(reply => {
     if (reply) {
+      const queueSpan = document.createElement('span');
+      queueSpan.textContent = 'queue';
+      queueSpan.classList.add('chip', 'orange', 'darken-2', 'white-text');
       collectionDiv.appendChild(queueSpan);
     }
   });
 
   isInCollection(userId, 'favorite', movieId).then(reply => {
     if (reply) {
+      const favoriteSpan = document.createElement('span');
+      favoriteSpan.textContent = 'favorite';
+      favoriteSpan.classList.add('chip', 'orange', 'darken-2', 'white-text');
       collectionDiv.appendChild(favoriteSpan);
     }
   });
 
+  // Creating element to contain contact info
   const contactDiv = document.createElement('div');
-  contactDiv.classList.add('col', 's4');
-  contactDiv.classList.add('secondary-content');
-  contactDiv.classList.add('right-align');
+  contactDiv.classList.add('col', 's4', 'right-align');
 
-  const toTelegram = document.createElement('a');
-  toTelegram.setAttribute('href', `https://web.telegram.org/#/im?p=${telegram}`);
-  toTelegram.setAttribute('target', '_blank');
-  // toTelegram.classList.add('secondary-content');
-
-  const ic = document.createElement('i');
-  ic.classList.add('material-icons');
-  ic.textContent = 'send';
-
-  const toMail = document.createElement('a');
-  toMail.setAttribute('href', `mailto:${email}`);
-  // toMail.classList.add('secondary-content');
-
-  const i = document.createElement('i');
-  i.classList.add('material-icons');
-  i.textContent = 'email';
-
-  toMail.appendChild(i);
-  toTelegram.appendChild(ic);
-
+  // Creating element for telegram contact
   if (telegram) {
+    const deviceType = getDeviceType();
+
+    const toTelegram = document.createElement('a');
+    toTelegram.classList.add('btn-floating', 'waves-effect', 'waves-light', 'telegram-btn');
+    if (deviceType === 'desktop') {
+      toTelegram.setAttribute('href', `https://web.telegram.org/#/im?p=${telegram}`);
+    } else {
+      toTelegram.setAttribute('href', `https://t.me/${telegram.slice(1)}`);
+    }
+    toTelegram.setAttribute('target', '_blank');
+
+    const telegramIcon = document.createElement('i');
+    telegramIcon.classList.add('material-icons');
+    telegramIcon.textContent = 'send';
+    toTelegram.appendChild(telegramIcon);
+
     contactDiv.appendChild(toTelegram);
   }
-  contactDiv.appendChild(toMail);
 
+  // Creating element for email contact
+  const toMail = document.createElement('a');
+  toMail.classList.add('btn-floating', 'waves-effect', 'waves-light');
+  toMail.setAttribute('href', `mailto:${email}`);
+
+  const emailIcon = document.createElement('i');
+  emailIcon.classList.add('material-icons');
+  emailIcon.textContent = 'email';
+
+  toMail.appendChild(emailIcon);
+
+  // Appending elements to Buddy item
+  contactDiv.appendChild(toMail);
   li.appendChild(nameSpan);
   li.appendChild(collectionDiv);
   li.appendChild(contactDiv);
-  // li.appendChild(queueSpan);
-  // li.appendChild(favoriteSpan);
 
-  // li.appendChild(toMail);
-
+  // Preparing the fragment
   fragment.appendChild(li);
 }
 
@@ -134,9 +141,6 @@ async function isInCollection(userId, collection, movieId) {
   return false;
 }
 
-// 'https://web.telegram.org/#/im?p=@IgromagClub';
-// 'https://t.me/larisa_antsifrova';
-
 const getDeviceType = () => {
   const ua = navigator.userAgent;
   if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
@@ -149,3 +153,7 @@ const getDeviceType = () => {
 };
 
 export { findBuddyBtnRef, findBuddy };
+
+// Examples of Telegram links
+// 'https://web.telegram.org/#/im?p=@IgromagClub';
+// 'https://t.me/larisa_antsifrova';
