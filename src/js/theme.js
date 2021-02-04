c4b389be880dfcd2909d9cb4730646e8e7dc3afb
arestus <arestus@gmail.com>
2021-02-04 19:14:51 +0200

    Add dark-theme style and gallery-item width

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,15 +1,21 @@
 const togleSwitchBtn = document.querySelector('[data-action-togle]');
 const mainRef = document.querySelector('main');
+const footerRef = document.querySelector('.site-footer');
+const headerRef = document.querySelector('header');
 
 togleSwitchBtn.addEventListener('click', switchTheme);
 
 function switchTheme(e) {
   if (e.target.textContent === 'brightness_6') {
     mainRef.classList.add('dark-theme');
+    footerRef.classList.add('dark-theme');
+    headerRef.classList.add('dark-theme');
     localStorage.setItem('Theme', 'DARK');
     togleSwitchBtn.firstElementChild.textContent = 'brightness_5';
   } else if (e.target.textContent === 'brightness_5') {
     mainRef.classList.remove('dark-theme');
+    footerRef.classList.remove('dark-theme');
+    headerRef.classList.remove('dark-theme');
     localStorage.removeItem('Theme', 'DARK');
     togleSwitchBtn.firstElementChild.textContent = 'brightness_6';
   }
@@ -17,5 +23,7 @@ function switchTheme(e) {
 
 if (localStorage.getItem('Theme') === 'DARK') {
   mainRef.classList.add('dark-theme');
+  footerRef.classList.add('dark-theme');
+  headerRef.classList.add('dark-theme');
   togleSwitchBtn.firstElementChild.textContent = 'brightness_5';
 }
