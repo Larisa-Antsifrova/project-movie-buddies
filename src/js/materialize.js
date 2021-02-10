4bab154471e5259eb501b12439d3b5ee458acb78
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-02-10 14:36:37 +0200

    Removed event listener on DOM content load.

--- src/js/materialize.js
+++ src/js/materialize.js
@@ -4,20 +4,18 @@ import { innerModalRef } from './show-details.js';
 
 // Setting up materialize components
 
-document.addEventListener('DOMContentLoaded', function () {
-  const tabs = document.querySelectorAll('.tabs');
-  M.Tabs.init(tabs);
+const tabs = document.querySelectorAll('.tabs');
+M.Tabs.init(tabs);
 
-  var modals = document.querySelectorAll('.modal');
-  M.Modal.init(modals);
+var modals = document.querySelectorAll('.modal');
+M.Modal.init(modals);
 
-  var elems = document.querySelectorAll('.sidenav');
-  M.Sidenav.init(elems, { edge: 'right', draggable: true });
+var elems = document.querySelectorAll('.sidenav');
+M.Sidenav.init(elems, { edge: 'right', draggable: true });
 
-  const detailsModal = document.getElementById('details-modal');
-  M.Modal.init(detailsModal, {
-    onCloseEnd: () => {
-      innerModalRef.innerHTML = '';
-    },
-  });
+const detailsModal = document.getElementById('details-modal');
+M.Modal.init(detailsModal, {
+  onCloseEnd: () => {
+    innerModalRef.innerHTML = '';
+  },
 });
